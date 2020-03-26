a4ac1bfcf69e12d527f4cd43759d1405c47ced44
mahesapama03adhiswara <mahesapamaadhiswara@gmail.com>
2020-03-27 01:39:51 +0800

    buat input string ke dalam variable

--- InputOutput.java
+++ InputOutput.java
@@ -1,5 +1,11 @@
+import java.util.scanner;
+
 public class InputOutput{
  public static void main(String [] args){
+  Scanner scanner  = new Scanner(System.in);
+  String namak
+  System.out.println("Masukkan nama anda : ");
+  nama = scanner.nextLine();
   
  }
 }
